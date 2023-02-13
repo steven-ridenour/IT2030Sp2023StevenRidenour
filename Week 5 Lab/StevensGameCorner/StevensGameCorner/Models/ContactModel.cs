5b13978ec32477493b84bb0c3ccfd536720e836a
ridenour719 <ridenour719@gmail.com>
2023-02-13 12:57:45 -0500

    contactmodel.cs created

---
+++ Week 5 Lab/StevensGameCorner/StevensGameCorner/Models/ContactModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StevensGameCorner.Models
+{
+    public class ContactModel
+    {
+        [Required(ErrorMessage = "Please enter your first name.")]
+        public string? FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your last name.")]
+        public string? LastName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your address.")]
+        public string? Address { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone number.")]
+        public string? Phone { get; set; }
+
+        [Required(ErrorMessage = "Please enter your city.")]
+        public string? City { get; set; }
+
+        [Required(ErrorMessage = "Please enter a message.")]
+        public string? Message { get; set; }
+
+    }
+}
