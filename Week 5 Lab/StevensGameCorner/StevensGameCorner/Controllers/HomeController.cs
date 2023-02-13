bd192f2a7188e0f3ef752f9d498b5108e5df920a
ridenour719 <ridenour719@gmail.com>
2023-02-13 12:45:49 -0500

    Home Controller About Page link added

--- Week 5 Lab/StevensGameCorner/StevensGameCorner/Controllers/HomeController.cs
+++ Week 5 Lab/StevensGameCorner/StevensGameCorner/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace StevensGameCorner.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
