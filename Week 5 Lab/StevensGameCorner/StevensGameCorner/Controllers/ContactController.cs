3f4e83ceca60efa348273d6243a91e13294de200
ridenour719 <ridenour719@gmail.com>
2023-02-13 14:56:42 -0500

    contact controller created

---
+++ Week 5 Lab/StevensGameCorner/StevensGameCorner/Controllers/ContactController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StevensGameCorner.Models;
+
+namespace StevensGameCorner.Controllers
+{
+    public class ContactController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(ContactModel model)
+        {
+            return View(model);
+        }
+    }
+
+}
+
