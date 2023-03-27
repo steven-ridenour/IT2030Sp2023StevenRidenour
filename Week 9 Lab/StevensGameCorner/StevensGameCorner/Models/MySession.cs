96058819cf13fa84ba8746d12940e833910c1e97
ridenour719 <ridenour719@gmail.com>
2023-03-26 18:58:16 -0700

    Added MySession.cs

---
+++ Week 9 Lab/StevensGameCorner/StevensGameCorner/Models/MySession.cs
@@ -0,0 +1,11 @@
+﻿namespace StevensGameCorner.Models
+{
+    public class MySession
+    {
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Course { get; set; }
+        public int FavNum { get; set; }
+
+    }
+}
