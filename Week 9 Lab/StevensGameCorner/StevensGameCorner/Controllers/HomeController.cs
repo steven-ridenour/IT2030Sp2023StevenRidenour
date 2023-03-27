a94f60ce6b768032c97cef0f57447215488887e7
ridenour719 <ridenour719@gmail.com>
2023-03-27 09:27:37 -0700

    Week 9 Lab Complete

--- Week 9 Lab/StevensGameCorner/StevensGameCorner/Controllers/HomeController.cs
+++ Week 9 Lab/StevensGameCorner/StevensGameCorner/Controllers/HomeController.cs
@@ -27,29 +27,22 @@ namespace StevensGameCorner.Controllers
         {
             return View();
         }
-        //### 6. Add two Tools action method to the HomeController –
-        //one will set the four variables and the other will get the four variables and have the view display them.
-        //In the first Tools action method, set the values for the four variables using FirstName, LastName, Course,and FavNum as the keys
-        //and your first name, your last name, our course number IT2030, and your favorite number
-        //as the corresponding values. When a user clicks the Display Session Variables button in the view, the assigned
-        //values should display in the View’s input boxes. The second Tools action method should clear the values in the input boxes.
         [HttpGet]
         public IActionResult Tools() 
-        {
-            HttpContext.Session.SetString("FirstName", "Steven");
-            HttpContext.Session.SetString("LastName", "Ridenour");
-            HttpContext.Session.SetString("Course", "IT2030");
-            HttpContext.Session.SetInt32("FavNum", 22);
-            return View();
-        }
-        [HttpPost]
-        [ActionName("Tools")]
-        public ViewResult ToolsClear()
         {
             HttpContext.Session.Remove("FirstName");
             HttpContext.Session.Remove("LastName");
             HttpContext.Session.Remove("Course");
             HttpContext.Session.Remove("FavNum");
+            return View();
+        }
+        [HttpPost]
+        public ViewResult Tools(MySession session)
+        {
+            HttpContext.Session.SetString("FirstName", "Steven");
+            HttpContext.Session.SetString("LastName", "Ridenour");
+            HttpContext.Session.SetString("Course", "IT2030");
+            HttpContext.Session.SetInt32("FavNum", 22);
             return View();
         }
         public IActionResult Privacy()
